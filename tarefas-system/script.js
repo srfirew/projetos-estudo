08a556bcf0006b517320a92e50e948ead4fca4c9
Gabriel Gomes <65258908+srfirew@users.noreply.github.com>
2024-11-14 10:03:26 -0300

    Update script.js

---
+++ tarefas-system/script.js
@@ -0,0 +1,11 @@
+const mainContainer = document.getElementById("main-container")
+const listaTarefas = document.getElementById("lista-tarefas")
+
+const criadorTarefa = document.getElementById("criador-tarefas")
+
+
+function criarTarefa(){
+    
+    criadorTarefa.innerHTML = "<p>oi</p>"
+    
+}
